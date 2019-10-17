3ce7f0af75ccf72c75f8d057e6607ceb2d7a2aab
Domonique <domonique.eccleston@gmail.com>
2019-10-17 01:10:54 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,20 +9,20 @@ window.onload = function(){
     myboard.forEach(i=>{i.className="square";});
 
   
-    function marksquare (marker){ 
-        var marksquare=[]; 
-        var value; 
-        myboard.click();
-        for ( var i=0; i<squarecount; i++){ 
-            value=myboard.innerHTML.className=".square.x";
-            if(value === marker){
+
+    myboard.click(function mark(mark){
+        var marksquare=[];  
+        var value;
+
+        for (var i=0; i<=9; i++){ 
+            value=myboard.innerHTML=".square.X";  
+            if (value===mark){
                 marksquare.push(i);
             }else{
-                value=myboard.innerHTML.className=".square.o"; 
+                value=myboard.innerHTML=".square.O";
                 marksquare.push(i);
             }
 
         }
-
-    }
+    });
 }
