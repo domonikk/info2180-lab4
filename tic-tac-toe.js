e483503e6cce1118c2ed73c7681c5019f28d303d
Domonique <domonique.eccleston@gmail.com>
2019-10-16 22:04:44 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -4,19 +4,9 @@ window.onload = function(){
     var body=document.getElementsByTagName("body"); 
     console.log(body); 
 
-    //
-    let board =document.getElementById("board");
-    let stat =document.getElementById("status");
-    let game =documemnt.getElementById("game"); 
-    
-
     // Adding squares to each div 
-    var mydiv=document.querySelectorAll("div.board");  
-    mydiv.classList.add('sqaure'); 
-    console.log(mydiv);
-    //mydiv.className='sqaure'; 
-    //console.log(mydiv.className);  
-    //console.log(board);
+    var myboard= document.getElementById("board").className="square";
+
     
 
     //
