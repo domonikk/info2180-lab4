053ff60f28e42a28f47a85fef128a94136edd5ce
Domonique <domonique.eccleston@gmail.com>
2019-10-20 22:38:08 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -11,7 +11,7 @@ window.onload = function(){
    // adding event to each element to display X and O 
     var element1= document.getElementsByClassName("square");
 
-    element1[0].onclick = function(){  
+    element1.onclick = function(){  
         element1[0].innerHTML="X"; 
         console.log("a click");
     };  
