3e1cdb8a33a6f4d154bda561ece405d3a4f48b2c
Domonique <domonique.eccleston@gmail.com>
2019-10-20 20:51:12 -0500

    Edited tic-tac-toe.js

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,27 +9,17 @@ window.onload = function(){
     myboard.forEach(i=>{i.className="square";});
 
    // adding event to each element to display X and O 
-     var element=document.getElementById("board").querySelectorAll("div");
+    var element=document.getElementById("board").querySelectorAll("div");
 
-     element.onlclick=fucntion(){ 
-         e1=element[0];
-         e2=element[1]; 
-         e3=element[2];
-         e4=element[3];
-         e5=element[4];
-         e6=element[5];
-         e7=element[6];
-         e8=element[7];
-         e9=element[8];
-        }; 
-
-     
-
-
-
-
-     
-
-
- 
+    element.onlclick=fucntion(){ 
+        e1=element[0].innerHTML="X";
+        e2=element[1].innerHTML="O"; 
+        e3=element[2].innerHTML="X";
+        e4=element[3].innerHTML="O"; 
+        e5=element[4].innerHTML="X";
+        e6=element[5].innerHTML="O"; 
+        e7=element[6].innerHTML="X";
+        e8=element[7].innerHTML="O"; 
+        e9=element[8].innerHTML="X";
+    }
 };
